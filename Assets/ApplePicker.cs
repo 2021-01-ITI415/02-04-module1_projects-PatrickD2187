9a0d1be6fda866b67b7764c8ee45f78741594cd3
PatrickD2187 <60270720+PatrickD2187@users.noreply.github.com>
2021-02-04 01:12:00 -0500

    Score Counter

--- Assets/ApplePicker.cs
+++ Assets/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -8,16 +9,39 @@ public class ApplePicker : MonoBehaviour
     public int numBaskets = 3;
     public float basketBottomY = -14;
     public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < numBaskets; i++)
+        basketList = new List<GameObject>();
+
+        for (int i = 0; i < numBaskets; i++)
         {
-            GameObject tBasketGO = Instantiate( basketPrefab ) as GameObject;
+            GameObject tBasketGO = Instantiate(basketPrefab) as GameObject;
             Vector3 pos = Vector3.zero;
-            pos.y = basketBottomY + ( basketSpacingY * i );
+            pos.y = basketBottomY + (basketSpacingY * i);
             tBasketGO.transform.position = pos;
+            basketList.Add(tBasketGO);
         }
     }
+    public void AppleDestroyed()
+    {
+        // Destroy all of the falling Apples
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+        }
+
+        // Destroy one of the Baskets
+        // Get the index of the last Basket in basketList
+        int basketIndex = basketList.Count - 1;
+        // Get a reference to that Basket GameObject
+        GameObject tBasketGO = basketList[basketIndex];
+        // Remove the Basket from the List and destroy the GameObject
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+    }  
+    
 }
