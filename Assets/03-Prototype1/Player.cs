4eaeeaa103e4edf2999cf2a8027a37b3390fc5ee
PatrickD2187 <60270720+PatrickD2187@users.noreply.github.com>
2021-02-17 12:25:44 -0500

    Player disapears when falling below the map

--- Assets/03-Prototype1/Player.cs
+++ Assets/03-Prototype1/Player.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playertrigger : MonoBehaviour
+public class Player : MonoBehaviour
 {
+
+    public static float bottomY = -20f;
     private int count;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (transform.position.y < bottomY)
+        {
+            Destroy(this.gameObject);
+
+            // Get a reference to the ApplePicker component of Main Camera
+            Prototype1 apScript = Camera.main.GetComponent<Prototype1>();
+
+            // Call the public PlayerDestroyed() method of apScript
+            apScript.PlayerDestroyed();
+        }
+
     }
     void OnTriggerEnter(Collider other)
     {
